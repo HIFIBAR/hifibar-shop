239d38e2d8128785293c4d61710eb403d9e08ec8
HIFIBAR <info@hifibar.fr>
2026-03-19 13:14:00 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/Images/CartesTV/:filename*',
+        destination: 'https://res.cloudinary.com/dopjvshmm/image/upload/CartesTV/:filename*',
+        permanent: true,
+      }
+    ]
+  }
 };
-
 module.exports = nextConfig;
