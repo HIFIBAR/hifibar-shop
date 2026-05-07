f473341747cef9bec8a568988d478e9f251994a6
HIFIBAR <info@hifibar.fr>
2026-05-07 14:26:21 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,19 +9,15 @@ const nextConfig = {
         source: '/Images/CartesTV/:filename*',
         destination: 'https://res.cloudinary.com/dopjvshmm/image/upload/CartesTV/:filename*',
         permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.hifibar.eu' }],
+        destination: 'https://hifibar.eu/:path*',
+        permanent: true,
       }
-    ]
+    ];
   }
 };
-module.exports = nextConfig;
 
-async redirects() {
-  return [
-    {
-      source: '/:path*',
-      has: [{ type: 'host', value: 'www.hifibar.eu' }],
-      destination: 'https://hifibar.eu/:path*',
-      permanent: true,
-    },
-  ];
-}
+module.exports = nextConfig;
