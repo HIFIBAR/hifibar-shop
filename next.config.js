fe1ccdee9baa31eaea0e2e671844d9a6154ecaae
HIFIBAR <info@hifibar.fr>
2026-05-07 17:36:06 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,12 +9,6 @@ const nextConfig = {
         source: '/Images/CartesTV/:filename*',
         destination: 'https://res.cloudinary.com/dopjvshmm/image/upload/CartesTV/:filename*',
         permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.hifibar.eu' }],
-        destination: 'https://hifibar.eu/:path*',
-        permanent: true,
       }
     ];
   }
