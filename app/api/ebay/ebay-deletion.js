180b056c93dd7077f3581f8e84604a6fe9c8fad9
HIFIBAR <info@hifibar.fr>
2026-05-07 13:43:16 +0200

    Update ebay-deletion.js

--- app/api/ebay/ebay-deletion.js
+++ app/api/ebay/ebay-deletion.js
@@ -4,7 +4,7 @@ export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const challengeCode = searchParams.get('challenge_code');
   
-  const VERIFICATION_TOKEN = "hifibar_ebay_token_2026";
+  const VERIFICATION_TOKEN = "hifibarSync2026EbayProductionToken";
   const ENDPOINT = "https://hifibar.eu/api/ebay/ebay-deletion";
 
   const hash = createHash('sha256')
